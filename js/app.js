81d7212c9501f068fcecc251136edfeb6075bd4d
Daniel <daniel2007dias@hotmail.com>
2025-06-20 18:25:29 -0300

    Comprar pista e botão comprar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 function comprar(){
-    let ingresso = document.getElementById('tipo-ingresso')
-    let qntd = document.getElementById('qtd').ariaValueMax;
+    let ingresso = document.getElementById('tipo-ingresso');
+    let qntd = document.getElementById('qtd').value;
+
+    if(ingresso.value == 'pista') {
+        comprarPista(qntd);
+    }
+}
+
+function comprarPista(qntd){
+    let qntdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qntd>qntdPista){
+        alert ('Quantidade indisponível')
+    } else{
+        qntdPista = qntdPista - qntd;
+        document.getElementById('qtd-pista').textContent = qntdPista
+        alert ('Compra realizada!')
+    }
 }
